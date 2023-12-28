7420bff51374f49f8232fd37b431536897ef2bb1
abhay8696 <kambleabhaygautam@gmail.com>
2023-12-28 18:10:21 +0530

    stopwatch changes3: edited time format, removed 1st 0 in mm

--- src/Stopwatch/Stopwatch.jsx
+++ src/Stopwatch/Stopwatch.jsx
@@ -20,22 +20,24 @@ const Stopwatch = () => {
         setTime(0)
     }
 
-    const displayTime = ()=>{
-        if(!time) return '0:00';
-        let mm = Math.floor(time/60);
-        let ss = time % 60;
-        let min = mm > 9 ? mm : `${mm}`;
+    const displayTime = seconds=>{
+        let min = Math.floor(seconds/60);
+        let ss = seconds % 60;
+
+        if(!seconds) {
+            min = 0;
+            ss = 0;
+        }
+        
         let sec = ss > 9 ? ss : `0${ss}`;
         return `${min} : ${sec}`
     }
     return (
         <div>
             <h1>Stopwatch</h1>
-            Time: {displayTime()}
-            <div>
-                <button onClick={()=> setRunning(prev=> !prev)}>{running ? "Stop" : "Start"}</button>
-                <button onClick={handleReset}>Reset</button>
-            </div>
+            <p>Time: {displayTime(time)}</p>
+            <button onClick={()=> setRunning(prev=> !prev)}>{running ? "Stop" : "Start"}</button>
+            <button onClick={handleReset}>Reset</button>
         </div>
     );
 };
