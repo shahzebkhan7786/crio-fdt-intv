f9a36f45e1e6b8abb6d10bba609ab78d46b97702
abhay8696 <kambleabhaygautam@gmail.com>
2023-12-29 22:58:03 +0530

    xCountiresSearch update:8

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -3,7 +3,7 @@ import "./card.css"
 const Card = (props) => {
     const { image, name } = props;
     return (
-        <div className='card'>
+        <div className='card' style={{flexDirection: 'column'}}>
             <img src={image} alt={`${name} flag`} />
             <h4>{name}</h4>
         </div>
